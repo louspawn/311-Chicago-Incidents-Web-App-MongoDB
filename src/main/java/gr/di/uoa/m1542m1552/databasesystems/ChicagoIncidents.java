b1f20a7dec43bd72c8b3d3a380fe8ea7b9f53d6e
Chris Lou <christos.lougiakis@gmail.com>
2019-01-15 19:37:09 +0200

    Created file structure

---
+++ src/main/java/gr/di/uoa/m1542m1552/databasesystems/ChicagoIncidents.java
@@ -0,0 +1,11 @@
+package gr.di.uoa.m1542m1552.databasesystems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChicagoIncidents {
+    public static void main(String [] argv){
+        SpringApplication.run(ChicagoIncidents.class, argv);
+    }
+}
